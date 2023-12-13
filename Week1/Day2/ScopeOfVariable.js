25f358bacdf00723de81e18b0aa13e51a93f0a2b
Gvijayasurya <113319874+Gvijayasurya@users.noreply.github.com>
2023-12-13 22:39:14 +0530

    Add files via upload

---
+++ Week1/Day2/ScopeOfVariable.js
@@ -0,0 +1,134 @@
+// //Data types
+// let a = 10;
+// let b = "String";
+// let c = true;
+// let d;
+// let e = "Bright";
+// e = null
+// console.log(a,b,c,d,e);
+
+// //Scope of Variable
+// const Id = 435
+// let tech = "Js"
+// var name = "vijay"
+// function Demo(){
+
+//     console.log(Id,name,tech);
+// }
+// Demo()
+// console.log(Id,"This is Global variable");
+
+// let Person = {
+//     name:"vijay",
+//     age:22,
+//     technology:"JS",
+// }
+// for(let key in Person){
+//     console.log(key);
+// }
+// let a =[1,2,3,4,5]
+// for(let key in a){
+//     console.log(a[key]);
+
+// }
+
+
+// //Conditional Statements
+// let a = 23
+// if(a%2==0){
+//     console.log("It is Even Number");
+// }
+// else{
+//     console.log("It is Odd Number");
+// }
+
+// let a = -1
+// if(a>0)
+// {
+//     console.log("It is a Positive number");
+// }
+// else if(a<0){
+//     console.log("It is a Negative number");
+// }
+// else{
+//     console.log("it is equal to zero");
+// }
+
+// let a = 10
+// if(a>0)
+// {
+//     if(a<=10)
+//     {
+//         console.log("It lies between 0 and 10");
+//     }
+//     else
+//     {
+//         console.log("It doesn't  lies between 0 and 10");
+//     }
+// }
+// else{
+//     console.log("It is a negative number");
+// }
+
+// //Looping Statements
+
+// for(let a = 1;a<10;a++)
+// {
+//     console.log(a);
+// }
+
+
+// let i = 0;
+// do {
+//     console.log("The number is "+ i);
+//     i++;
+//   }
+//   while (i == 10);
+
+
+
+// //Creating Object using Literal
+// let Company = {
+//     name:"Revature",
+//     Year: 2021,
+//     location:"chennai",
+// }
+// for(let key in Company)
+// {
+//     console.log(key,Company[key]);
+// }
+// //Creating Object using Class
+// class Laptop{
+//     constructor(brand,Gen,ram){
+//         this.brand=brand
+//         this.Gen = Gen
+//         this.Ram = ram
+//     }
+// }
+// let L = new Laptop("Dell","i5","512-SSD")
+// console.log(L.brand,L.Gen,L.Ram);
+// L.size=14.5
+// console.log(L.size);
+// console.log(L);
+
+// //Pushing Objects as elements of array
+// let arr = []
+// arr.push(Company)
+// arr.push(L)
+// console.log(arr);
+
+
+// // Iterating Array using for of loop
+// for(let key of arr)
+// {
+//     console.log(key);
+// }
+
+// //Iterating Object using for in loop
+
+// for(let key in L)
+// {
+//     console.log(key);
+// }
+
+
