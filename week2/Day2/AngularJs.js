e26e00c912e41db1004cc2b463d18161d8e44ca5
Gvijayasurya <113319874+Gvijayasurya@users.noreply.github.com>
2023-12-19 18:06:10 +0530

    Add files via upload

---
+++ week2/Day2/AngularJs.js
@@ -0,0 +1,12 @@
+
+let pro = angular.module("Demo",[])
+
+    pro.controller("usercontroller",function(){
+    this.uname="vijay"
+    this.uid=435
+    this.status="Online"
+})
+
+pro.controller("companycontroller",function(){
+
+})
